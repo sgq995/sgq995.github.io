3cf98785f489fd9c16092d7f18f49877236ddb2d
Sebastian G. Q <sgq995@gmail.com>
2020-03-24 20:53:00 -0500

    feat: Added font-awesome resources for React

--- src/Nav.js
+++ src/Nav.js
@@ -1,35 +1,64 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
 import MenuIcon from './icon-menu.svg';
 import './Nav.css';
 
-function Nav() {
+function Nav(props) {
+  const LIGHT_THEME = 'light';
+  const NIGHT_THEME = 'night';
+
+  const [theme, setTheme] = React.useState(LIGHT_THEME);
+
   const items = [
     'Blog',
     // 'Portafolio',
     'Projects',
     'About me',
-    'Contact',
-    'Test'
+    'Contact'
   ];
   
-  const makeNavItem = (text, link) => {
+  const makeIconItem = (key, icon, iconProps, link, onclick) => {
     return (
-      <li className="nav-item" key={text.toLowerCase()}>
-        <a href={link ? link : '#'} className="nav-link">{text}</a>
+      <li className="nav-item" key={key}>
+        <a href={link ? link : '#'} className="nav-link" onClick={onclick}>
+          <FontAwesomeIcon icon={icon} props={iconProps} />
+        </a>
       </li>
     );
+  };
+
+  const makeLinkItem = (text, link, onclick) => {
+    return (
+      <li className="nav-item" key={text.toLowerCase()}>
+        <a href={link ? link : '#'} className="nav-link" onClick={onclick}>{text}</a>
+      </li>
+    );
+  };
+
+  const makeImageItem = (link, src, alt, onclick) => {
+    return (
+      <li className="nav-item" key={alt.toLowerCase()}>
+        <a href={link} className="nav-link">
+          <img src={src} alt={alt} style={{height: '1em'}} onClick={onclick} />
+        </a>
+      </li>
+    );
+  }
+
+  const themeChangeHandle = () => {
+    setTheme(theme === LIGHT_THEME ? NIGHT_THEME : LIGHT_THEME);
   };
 
   return (
     <nav className="navbar">
       <ul className="navbar-nav">
-        <li className="nav-item" key="burger">
-          <a href="#" className="nav-link">
-            <img src={MenuIcon} alt="burger" width="32" />
-          </a>
-        </li>
-        {items.map(item => makeNavItem(item))}
+        {/* {makeImageItem('#', MenuIcon, 'Menu')} */}
+        {makeIconItem('bars', faBars)}
+        {items.map(item => makeLinkItem(item))}
+        {makeLinkItem(theme === LIGHT_THEME ? 'Night' : 'Light', '#', themeChangeHandle)}
       </ul>
     </nav>
   );
