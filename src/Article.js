48b16608e4e0b87ed74930420d7e6f6a1b3ed870
Sebastian G. Q <sgq995@gmail.com>
2020-03-29 11:51:35 -0500

    feat: Added style to article

--- src/Article.js
+++ src/Article.js
@@ -1,21 +1,31 @@
 import React from 'react';
 
+import './Article.css'
+
 function Article(props) {
   const { title, subtitle, content, date } = props;
 
+  const wordList = content.split(' ');
+  const summary = wordList.slice(0, 10).join(' ') + '...';
+
   return (
-    <article>
+    <article className="article">
       <header>
-        <h1>{title}</h1>
-        <h3>{subtitle}</h3>
+        <h1 className="article-title">{title}</h1>
+        <h3 className="article-subtitle">{subtitle}</h3>
       </header>
       <section>
-        <p>
-          {content}
-        </p>
+        <details>
+          <p>
+            {content}
+          </p>
+          <summary>
+            {summary}
+          </summary>
+        </details>
       </section>
-      <footer>
-        <p>Posted on {date}</p>
+      <footer className="article-footer">
+        Posted on {date}
       </footer>
     </article>
   );
