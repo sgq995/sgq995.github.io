c12e2787aea84d4625f791fe54773eb3512347ac
Sebastian G. Q <sgq995@gmail.com>
2020-09-19 17:57:28 -0500

    feat: Added extra typography font

--- src/Typography.js
+++ src/Typography.js
@@ -4,9 +4,11 @@ import ThemeContext from './ThemeContext';
 import './Typography.css'
 
 function Typography(props) {
-  const { children, component='span', ...opts } = props;
+  const { children, component='span', special, ...opts } = props;
 
   // const theme = React.useContext(ThemeContext);
+  const className = opts.className ? opts.className : '';
+  opts.className = special ? `${className} typography-special` : className;
 
   return React.createElement(component, opts, children);
 }
