7ccfd5587f0c7f9079fefad08c1f7181788044d8
Sebastian G. Q <sgq995@gmail.com>
2021-01-10 16:55:41 -0500

    feat: Modified link typo for posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -15,9 +15,11 @@ function Post(props: React.ComponentProps<React.FC>) {
         <h2 className="Post__headline1">Lorem ipsum</h2>
         <h3 className="Post__headline2">Dolor sit amet</h3>
 
-        <Typography className="typography-caption">
-          <Link href="#">tags (1)</Link>
-        </Typography>
+        <Link href="#">
+          <Typography className="Post__caption">
+            tags (1)
+          </Typography>
+        </Link>
       </hgroup>
 
       <p className="Post__body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam felis lorem, pulvinar ac neque in, tincidunt feugiat libero. Sed laoreet at mi at convallis. Cras in tincidunt tortor, nec volutpat est. Sed pretium sodales nisl, ac hendrerit libero luctus sit amet. Nam rutrum tempus justo, ac semper sapien. Suspendisse imperdiet enim nulla, nec euismod ipsum scelerisque a. Aliquam fermentum accumsan eros. Cras nibh massa, faucibus sed orci ac, faucibus faucibus tortor. Vivamus placerat blandit luctus. Maecenas sit amet arcu gravida, volutpat justo nec, ultricies velit. Aliquam erat volutpat. Integer felis nunc, luctus id pretium nec, lobortis id tortor. Phasellus pretium non libero et dictum.</p>
