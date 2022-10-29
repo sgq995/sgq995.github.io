d0cbe3eb81ec904dd3806e5644b12c269238e7a2
Sebastian G. Q <sgq995@gmail.com>
2022-10-29 10:27:25 -0500

    feat: Transitions added

--- static/js/post-view.js
+++ static/js/post-view.js
@@ -1,17 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
   const menuRoot = document.querySelector('.menu__root');
 
-  const menuOpenButton = document.querySelector('.menu__open');
-  menuOpenButton.addEventListener('click', () => {
+  const handleNavRootShow = () => {
+    navContainer.classList.remove('overflow-hidden');
+
+    navRoot.removeEventListener('transitionend', handleNavRootShow);
+  }
+
+  const handleOpenNavbar = () => {
     document.body.classList.add('overflow-hidden');
-    menuRoot.classList.add('hidden');
-    navRoot.classList.toggle('hidden');
-  });
+    menuRoot.classList.add('menu__root--hidden');
+    navRoot.classList.remove('hidden');
+    navRoot.classList.add('nav__root--shown');
+    navContainer.classList.add('overflow-hidden');
+
+
+    navRoot.addEventListener('transitionend', handleNavRootShow);
+  }
+
+  const menuOpenButton = document.querySelector('.menu__open');
+  menuOpenButton.addEventListener('click', handleOpenNavbar);
+
+  const handleNavRootHide = () => {
+    navRoot.classList.add('hidden');
+    navContainer.classList.remove('overflow-hidden');
+
+    navRoot.removeEventListener('transitionend', handleNavRootHide);
+  };
 
   const handleCloseNavbar = () => {
     document.body.classList.remove('overflow-hidden');
-    navRoot.classList.add('hidden');
-    menuRoot.classList.toggle('hidden');
+    menuRoot.classList.toggle('menu__root--hidden');
+    navRoot.classList.remove('nav__root--shown');
+    navContainer.classList.add('overflow-hidden');
+
+    navRoot.addEventListener('transitionend', handleNavRootHide);
   };
 
   const navRoot = document.querySelector('.nav__root');
@@ -21,8 +44,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  const navContainer = document.querySelector('.nav__container');
+
   const navbar = document.querySelector('.navbar');
 
   const navbarCloseButton = document.querySelector('button.navbar__close');
   navbarCloseButton.addEventListener('click', handleCloseNavbar);
+
+  let lastScrollY = 0;
+  let ticking = false;
+  document.addEventListener('scroll', (e) => {
+    const currentScrollY = window.scrollY;
+
+    if (!ticking) {
+      requestAnimationFrame(() => {
+        const delta = lastScrollY - currentScrollY;
+
+        if (currentScrollY > 16) {
+          menuOpenButton.classList.add('menu__open--detached');
+          if (delta > 0) {
+            // Scrolling up
+            menuRoot.classList.remove('menu__root--hidden');
+          } else {
+            // Scrolling down
+            menuRoot.classList.add('menu__root--hidden');
+          }
+        } else {
+          menuRoot.classList.remove('menu__root--hidden');
+          menuOpenButton.classList.remove('menu__open--detached');
+        }
+
+        lastScrollY = currentScrollY;
+        ticking = false;
+      });
+
+      ticking = true;
+    }
+  });
 });
