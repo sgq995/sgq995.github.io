a8119a7db1fefa46077e47b5c41676af78181167
Sebastian G. Q <sgq995@gmail.com>
2023-02-15 21:15:39 -0500

    chore: Styles

--- themes/minimo/src/scripts/sidebar.js
+++ themes/minimo/src/scripts/sidebar.js
@@ -13,78 +13,16 @@ const setAriaExpanded = (items, value) => {
 }
 
 const hideSidebar = () => {
+  body.classList.remove('sidebar-is-shown')
   sidebar.classList.remove('toggled')
   setAriaExpanded([sidebar, expandButton, collapseButton], false)
 }
 
 const showSidebar = () => {
+  body.classList.add('sidebar-is-shown')
   sidebar.classList.add('toggled')
   setAriaExpanded([sidebar, expandButton, collapseButton], true)
   sidebar.focus()
-}
-
-let windowWidth,
-  windowHeight,
-  bodyHeight,
-  sidebarHeight,
-  windowPos,
-  lastWindowPos = 0,
-  top = false,
-  bottom = false,
-  topOffset = 0,
-  sidebarOffsetTop,
-  resizeTimer
-
-const resizeHandler = () => {
-  windowWidth = window.innerWidth
-  windowHeight = window.innerHeight
-}
-
-const scrollHandler = () => {
-  windowPos = window.scrollY
-  bodyHeight = body.offsetHeight
-  sidebarHeight = sidebar.offsetHeight
-  sidebarOffsetTop = Math.round(windowPos + sidebar.getBoundingClientRect().top)
-
-  if (sidebarHeight > windowHeight) {
-    if (windowPos > lastWindowPos) {
-      if (top) {
-        top = false
-        topOffset = sidebarOffsetTop > 0 ? sidebarOffsetTop : 0
-        sidebar.setAttribute('style', `top: ${topOffset}px;`)
-      } else if (
-        !bottom &&
-        windowPos + windowHeight > sidebarHeight + sidebarOffsetTop &&
-        sidebarHeight < bodyHeight
-      ) {
-        bottom = true
-        sidebar.setAttribute('style', 'position: fixed; bottom: 0;')
-      }
-    } else if (windowPos < lastWindowPos) {
-      if (bottom) {
-        bottom = false
-        topOffset = sidebarOffsetTop > 0 ? sidebarOffsetTop : 0
-        sidebar.setAttribute('style', `top: ${topOffset}px;`)
-      } else if (!top && windowPos < sidebarOffsetTop) {
-        top = true
-        sidebar.setAttribute('style', 'position: fixed;')
-      }
-    } else {
-      top = bottom = false
-      topOffset = sidebarOffsetTop ? sidebarOffsetTop : 0
-      sidebar.setAttribute('style', `top: ${topOffset}px;`)
-    }
-  } else if (!top) {
-    top = true
-    sidebar.setAttribute('style', 'position: fixed;')
-  }
-
-  lastWindowPos = windowPos
-}
-
-const resizeAndScrollHandler = () => {
-  resizeHandler()
-  scrollHandler()
 }
 
 const initSidebarMenu = () => {
@@ -114,14 +52,6 @@ export const initSidebar = () => {
   expandButton.addEventListener('click', showSidebar)
   collapseButton.addEventListener('click', hideSidebar)
   overlay.addEventListener('click', hideSidebar)
-
-  window.addEventListener('scroll', scrollHandler)
-  window.addEventListener('resize', () => {
-    clearTimeout(resizeTimer)
-    resizeTimer = setTimeout(resizeAndScrollHandler, 500)
-  })
-
-  resizeAndScrollHandler()
 
   if (sidebarMenu) initSidebarMenu()
 }
