f1943236941ee8303e365e92d54843e16785566c
Sebastian G. Q <sgq995@gmail.com>
2023-05-10 19:42:27 -0500

    chore: integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(), partytown(), compress()]
 });
