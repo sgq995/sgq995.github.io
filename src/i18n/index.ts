db3a968706a3c8b32df35744857c30b8a17febb3
Sebastian G. Q <sgq995@gmail.com>
2023-05-18 18:53:11 -0500

    feat: localization

---
+++ src/i18n/index.ts
@@ -0,0 +1,28 @@
+import en from './en.json';
+import es from './es.json';
+
+export const defaultLang = 'es';
+
+export const locales = {
+  en,
+  es,
+};
+
+export type Locale = keyof typeof locales;
+
+export type LocaleKey = keyof (typeof locales)[Locale];
+
+export function getLangFromUrl(url: URL) {
+  const lang = url.host.split('.')[0];
+  if (lang in locales) {
+    return lang as Locale;
+  }
+
+  return defaultLang;
+}
+
+export function getTransalations(lang: Locale) {
+  return function t(key: LocaleKey) {
+    return locales[lang][key] ?? locales[defaultLang][key];
+  };
+}
