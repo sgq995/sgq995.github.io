7df8a3f169519ea079fb967b3d9f6e732925d1a8
Sebastian G. Q <sgq995@gmail.com>
2023-05-18 18:52:35 -0500

    chore: deps

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require("prettier-plugin-astro"),
+    require("prettier-plugin-import-sort"),
+    require("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
+};
