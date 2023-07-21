ca4b562b4b3ccd79478753c71384e7000359579a
Sebastian G. Q <sgq995@gmail.com>
2023-07-20 21:33:24 -0500

    feat: dark theme

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -13,7 +13,7 @@ const FloatingActionButton: ParentComponent<FloatingActionButtonProps> = (
   return (
     <button
       {...props}
-      class="fixed bottom-8 left-8 z-50 rounded-full bg-sky-500 p-4 text-white shadow-lg"
+      class="fixed bottom-8 left-8 z-50 rounded-full bg-sky-500 p-4 text-white shadow-lg dark:bg-orange-500 dark:shadow-slate-800"
     >
       {icon()}
     </button>
---
+++ src/components/ThemeButton.tsx
@@ -0,0 +1,57 @@
+import { createSignal, type Component, Show, onMount } from "solid-js";
+import { TbMoon, TbSun } from "solid-icons/tb";
+
+export const ThemeButton: Component = () => {
+  const [theme, setTheme] = createSignal<"dark" | "light">("light");
+
+  const setUserTheme = (theme: "dark" | "light") => {
+    if (theme === "light") {
+      document.documentElement.classList.add("light");
+      document.documentElement.classList.remove("dark");
+
+      localStorage.setItem("theme", "light");
+      setTheme("light");
+    } else if (theme === "dark") {
+      document.documentElement.classList.remove("light");
+      document.documentElement.classList.add("dark");
+
+      localStorage.setItem("theme", "dark");
+      setTheme("dark");
+    }
+  };
+
+  onMount(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (
+      storedTheme === "dark" ||
+      (storedTheme === null &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setUserTheme("dark");
+    } else {
+      setUserTheme("light");
+    }
+  });
+
+  const handleButtonClick = () => {
+    if (theme() === "dark") {
+      setUserTheme("light");
+    } else {
+      setUserTheme("dark");
+    }
+  };
+
+  return (
+    <button
+      class="rounded-full border border-gray-300 p-2 shadow-lg dark:border-slate-800 dark:bg-slate-700 dark:shadow-slate-800"
+      onClick={handleButtonClick}
+    >
+      <Show when={theme() === "dark"}>
+        <TbSun class="h-6 w-6 fill-none stroke-white stroke-2" />
+      </Show>
+      <Show when={theme() === "light"}>
+        <TbMoon class="h-6 w-6 fill-none stroke-black stroke-2" />
+      </Show>
+    </button>
+  );
+};
